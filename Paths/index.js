b71919163f178e579107fd62cd40df56f64036b0
inehk <yynk83@gmail.com>
2017-12-22 22:57:53 +0100

    correctifs SVG

--- Paths/index.js
+++ Paths/index.js
@@ -16,11 +16,11 @@ Utiliser les poids pour les positionnement (si nouveau bloc, peu de poids pour n
 
 
 
-
-
-
-
 */
+
+
+
+
 
 var svg = document.getElementById('main');
 var width = window.innerWidth;
@@ -36,25 +36,25 @@ svg.addEventListener('contextmenu', function(event) {
 var subjects_ = [{
   id: 1,
   name: 'Maximum likelihood estimator',
-  desc: '',
+  desc: 'estimateur statistique pour inférer les paramètres d\'une distribution de probabilité d\'un échantillon donné',
   progress: 10,
   dependencies: [2, 3]
 },{
   id: 2,
   name: 'Random variable',
-  desc: '',
+  desc: 'discrète, continue, ...',
   progress: 100,
   dependencies: [/*1, 3*/]
 },{
   id: 3,
   name: 'Probability Density function',
-  desc: '',
+  desc: 'Représentation d\'une loi de probabilité',
   progress: 80,
   dependencies: [/*2, 1*/]
 },{
   id: 4,
-  name: 'Test',
-  desc: '',
+  name: 'Pearson\'s Chi-Squared test',
+  desc: 'test d\'indépendence',
   progress: 80,
   dependencies: [2, 3]
 }];
@@ -72,83 +72,138 @@ function getRandomPosition() {
   return {x: parseInt(Math.random()*width*0.75)+50, y: parseInt(Math.random()*height*0.75)+25};
 }
 
+var currentSelected = null, currentX, currentY; // l'objet que l'on est en train de déplacer
+
 function createBox(subject) {
-  var boxGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
 
-    var boxWidth = (subject.name.length*7.5+30);
-    //var pos = getRandomPosition();
-    //subject.x = pos.x;
-    //subject.y = pos.y;
-    subject.boxWidth = boxWidth;
-    subject.boxGroup = boxGroup;
+  var boxGroup = document.getElementById('subject_' + subject.id);
+  if(boxGroup) {
+    boxGroup.setAttribute("transform", "translate("+subject.x+", "+subject.y+") rotate(0)");
+  }else{
 
-    // Boite
-    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    rect.setAttribute("id", "box");
-    rect.setAttribute("fill", "beige");
-    rect.setAttribute("stroke", "black");
-    rect.setAttribute("x", subject.x+"px");
-    rect.setAttribute("y", subject.y+"px");
-    rect.setAttribute("width", boxWidth+"px");
-    rect.setAttribute("height", "50px");
-    rect.setAttribute("pointer-events", "inherit");
-    rect.setAttribute('draggable', true);
-    boxGroup.appendChild(rect);
+    boxGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    boxGroup.setAttribute("transform", "translate("+subject.x+", "+subject.y+") rotate(0)");
+    boxGroup.setAttribute("subjectId", subject.id);
+    boxGroup.setAttribute("id", "subject_"+subject.id);
 
-    subject.rect = rect;
 
-    // Text name
-    var textName = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    textName.innerHTML = subject.id+". "+subject.name;
-    textName.setAttribute("x", (subject.x+9)+"px");
-    textName.setAttribute("y", (subject.y+19)+"px");
-    textName.setAttribute("fill", "black");
-    textName.setAttribute("stroke", "black");
-    textName.setAttribute("font-size", "10pt");
-    textName.setAttribute("family", "sans serif");
-    boxGroup.appendChild(textName);
+      var boxWidth = (subject.name.length*7.5+30);
+      //var pos = getRandomPosition();
+      //subject.x = pos.x;
+      //subject.y = pos.y;
+      subject.boxWidth = boxWidth;
+      subject.boxGroup = boxGroup;
 
-    subject.textName = textName;
+      // Boite
+      var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+      rect.setAttribute("id", "box");
+      rect.setAttribute("fill", "beige");
+      rect.setAttribute("stroke", "black");
+      rect.setAttribute("width", boxWidth+"px");
+      rect.setAttribute("height", "50px");
+      rect.setAttribute("pointer-events", "all");
+      boxGroup.appendChild(rect);
+      subject.rect = rect;
 
-    // Text description
+      // Text name
+      var textName = document.createElementNS("http://www.w3.org/2000/svg", "text");
+      textName.innerHTML = subject.id+". "+subject.name;
+      textName.setAttribute("x", "9px");
+      textName.setAttribute("y", "19px");
+      textName.setAttribute("fill", "black");
+      textName.setAttribute("stroke", "black");
+      textName.setAttribute("font-size", "10pt");
+      textName.setAttribute("pointer-events", "none");
+      textName.setAttribute("family", "sans serif");
+      boxGroup.appendChild(textName);
 
-    // Rect pourcentage progression
+      subject.textName = textName;
 
-  // EVENTS sur les box
-  boxGroup.addEventListener('mouseover', function(e) { e.target.parentNode.querySelector("rect").setAttribute("stroke-width", 5); });
-  boxGroup.addEventListener('mouseout', function(e) { e.target.parentNode.querySelector("rect").setAttribute("stroke-width", 1); });
-  boxGroup.addEventListener('dragstart', function(e) {
-    console.log("dragstart", e);
-    console.log(e.target.parentNode);
-  });
-  boxGroup.addEventListener('drag', function(e) {
-    console.log("drag", e);
-  });
-  boxGroup.addEventListener('dragend', function(e) {
-    console.log("dragend", e);
-    e.target.parentNode.setAttribute("x", e.clientX);
-    e.target.parentNode.setAttribute("y", e.clientY);
-  });
-  boxGroup.setAttribute('class', 'bloc');
-  boxGroup.setAttribute('draggable', true);
-  svg.appendChild(boxGroup);
+      // Text description
 
+      // Rect pourcentage progression
+
+    // EVENTS sur les box
+    boxGroup.addEventListener('mouseover', function(e) { e.target.parentNode.querySelector("rect").setAttribute("stroke-width", 5); });
+    boxGroup.addEventListener('mouseout', function(e) { e.target.parentNode.querySelector("rect").setAttribute("stroke-width", 1); });
+
+    boxGroup.addEventListener('mousedown', function(e) {
+      currentSelected = e.target.tagName == 'rect' ? e.target.parentNode : e.target;
+      currentX = e.clientX;
+      currentY = e.clientY;
+    });
+    svg.addEventListener('mousemove', function(e) {
+      if(currentSelected) {
+        var transform = currentSelected.getAttribute("transform");
+        var myRegex = /translate\(([0-9\.]*), ([0-9\.]*)\)/g;
+        var values = myRegex.exec(transform);
+        if(values && values.length > 1) {
+          var dx = parseFloat(values[1]) + e.clientX - currentX;
+          var dy = parseFloat(values[2]) + e.clientY - currentY;
+          currentX = e.clientX;
+          currentY = e.clientY;
+          if(!isNaN(dx) && !isNaN(dy) && dx > 0 && dy > 0) {
+            currentSelected.setAttribute("transform", "translate("+parseInt(dx)+", "+parseInt(dy)+") rotate(0)");
+
+            // On efface toutes les lignes
+            /*
+            var all = svg.querySelectorAll("line");
+            var forEach = Array.prototype.forEach;
+            forEach.call(all, function(line){
+              svg.removeChild(line);
+            });
+            */
+
+            Object.values(subjects).forEach(function(s){
+              if(s.id == currentSelected.getAttribute("subjectId")) {
+                s.x = parseInt(dx);
+                s.y = parseInt(dy);
+              }
+              createLine(s);
+            });
+
+
+            Object.values(subjects).forEach(function(s, idx){
+              createBox(s);
+            });
+          }
+        }
+      }
+    });
+    document.body.addEventListener('mouseup', function(e) {
+      console.log("mouseUP!!", e.target);
+      currentSelected = null;
+    });
+    boxGroup.setAttribute('class', 'bloc');
+
+    svg.appendChild(boxGroup);
+  }
   return boxGroup;
 }
 
 function createLine(subject) {
   subject.dependencies.forEach(function(dep) {
-    var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-    line.setAttribute("x1", subject.x+(subject.name.length*7.5+30)/2);
-    line.setAttribute("y1", subject.y+10);
-    line.setAttribute("x2", subjects[dep].x + subjects[dep].name.length*9/2);
-    line.setAttribute("y2", subjects[dep].y + 10);
-    line.setAttribute("stroke", "black");
-    line.setAttribute("stroke-width", 2);
-    line.setAttribute("marker-end", 'url(#head)');
-    line.addEventListener('mouseover', function(e) { e.target.setAttribute("stroke-width", 5); });
-    line.addEventListener('mouseout', function(e) { e.target.setAttribute("stroke-width", 1); });
-    svg.appendChild(line);
+    var id = "from_"+subject.id+"_to_"+subjects[dep].id;
+    var line = document.getElementById(id);
+    if(line) {
+      line.setAttribute("x2", subject.x+(subject.name.length*7.5+30)/2);
+      line.setAttribute("y2", subject.y+10);
+      line.setAttribute("x1", subjects[dep].x + (subjects[dep].name.length*7.5+30)/2);
+      line.setAttribute("y1", subjects[dep].y + 10);
+    }else{
+      var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+      line.setAttribute("x2", parseInt(subject.x+(subject.name.length*7.5+30)/2));
+      line.setAttribute("y2", subject.y+10);
+      line.setAttribute("x1", parseInt(subjects[dep].x + (subjects[dep].name.length*7.5+30)/2));
+      line.setAttribute("y1", subjects[dep].y + 10);
+      line.setAttribute("stroke", "black");
+      line.setAttribute("stroke-width", 2);
+      line.setAttribute("marker-start", 'url(#head)');
+      line.setAttribute("id", id);
+      line.addEventListener('mouseover', function(e) { e.target.setAttribute("stroke-width", 5); });
+      line.addEventListener('mouseout', function(e) { e.target.setAttribute("stroke-width", 1); });
+      svg.appendChild(line);
+    }
   });
 }
 
@@ -171,16 +226,17 @@ function createLine(subject) {
  *
  */
 
- REPULSION_CONSTANT = 1; // Coulomb ... ?
- ATTRACTION_CONSTANT = 10;
- SPRING_LENGTH = 10000; // plus petit = ressort plus "compacte", mais pb si trop compacte ça explose ???!!...
+ REPULSION_CONSTANT = 0.1; // Coulomb ... ?
+ ATTRACTION_CONSTANT = 100;
+ SPRING_LENGTH = 1000; // plus petit = ressort plus "compacte", mais pb si trop compacte ça explose ???!!...
 
 function distance(a, b) {
-  return Math.sqrt(
+  //console.log("distance:", a.x, a.y, b.x, b.y);
+  return parseInt(Math.sqrt(
     Math.pow(a.x - b.x, 2)
     +
     Math.pow(a.y - b.y, 2)
-  );
+  ));
 }
 
 ForceLayout = (function() {
@@ -191,7 +247,7 @@ ForceLayout = (function() {
 
   function doLayout() {
     var i = 0;
-    while(i < 10) {
+    while(i < 1000) {
       var totalDisplacement = 0;
 
       // Pour chaque noeud : on va calculer la force à y appliquer
@@ -234,8 +290,8 @@ ForceLayout = (function() {
         }
 
         // d'y mettre ici ça fait quoi de mieux ???!!!
-        node.x += node.velocityX*0.1/* v*dt */;
-        node.y += node.velocityY*0.1/* v*dt */;
+        node.x += node.velocityX*1/* v*dt */;
+        node.y += node.velocityY*1/* v*dt */;
 
       });
 
@@ -260,14 +316,16 @@ ForceLayout = (function() {
 
       //console.log("totalDisplacement:", totalDisplacement);
 
+      document.getElementById('totalDisplacement').innerHTML = totalDisplacement + "--" + i;
+
       // fin : plus rien ne bouge
-      if(totalDisplacement < 1E-9) {
+      if(totalDisplacement < 100) {
         break;
       }
 
       i++;
     }
-    //console.log("i=", i);
+    console.log("i=", i);
   };
 
   function getBearingAngle(node1, node2) {
@@ -302,13 +360,34 @@ ForceLayout.setGraphObject(subjects);
 ForceLayout.doLayout();
 
 
+function displayLines() {
+  /*var all = svg.querySelectorAll("line");
+  var forEach = Array.prototype.forEach;
+  forEach.call(all, function(line){
+    svg.removeChild(line);
+  });*/
+  Object.values(subjects).forEach(function(s){
+    createLine(s);
+  });
+}
 
-Object.values(subjects).forEach(function(s){
-  createLine(s);
-});
-
-
+displayLines();
 
 Object.values(subjects).forEach(function(s, idx){
   createBox(s);
 });
+
+var arrangeBtn = document.createElement('button');
+arrangeBtn.innerHTML = 'Arrange boxes';
+arrangeBtn.addEventListener('click', function() {
+  ForceLayout.setGraphObject(subjects);
+  ForceLayout.doLayout();
+
+  displayLines();
+
+  Object.values(subjects).forEach(function(s, idx){
+    createBox(s);
+  });
+
+})
+document.getElementById("controls").appendChild(arrangeBtn);
